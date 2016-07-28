63d2c2afa246599bb40bd475eedc56c98a38e157
Will Fisher <will@gina.alaska.edu>
2016-07-28 15:27:34 -0800

    build using latest cookbook

--- cookbooks/nace-dev/recipes/default.rb
+++ cookbooks/nace-dev/recipes/default.rb
@@ -29,62 +29,6 @@ link '/opt/solr-4.10.4/example/solr/collection1/conf/schema.xml' do
   notifies :restart, 'service[solr]', :immediately
 end
 
-include_recipe 'postgresql::ruby'
-
-pg_connection_info = { host: '127.0.0.1' }
-
-postgresql_database_user 'ckan_default' do
-  connection pg_connection_info
-  password 'pass'
-  action :create
-end
-
-postgresql_database 'ckan_default' do
-  connection pg_connection_info
-  encoding 'UTF8'
-  owner 'ckan_default'
-end
-
 directory '/etc/solr/conf' do
   recursive true
-end
-
-template '/etc/ckan/default/production.ini' do
-  source 'production.ini.erb'
-  variables ({
-    'port' => '5000',
-    'site_url' => node['ckan']['site_url'],
-    'session_secret' => '/LQ1h6/Sl0EFEF1maYhFs0Sxo',
-    'instance_uuid' => '200e5ca3-cffd-47aa-a93e-4c40bb81ce2c',
-    'postgresql_url' => "postgresql://#{node.ckan.db_username}:#{node.ckan.db_password}@#{node.ckan.db_address}/#{node.ckan.db_name}",
-    'postgresql_datastore_write_url' => "postgresql://#{node.ckan.db_username}:#{node.ckan.db_password}@#{node.ckan.db_address}/#{node.ckan.db_datastore_name}",
-    'postgresql_datastore_read_url' => "postgresql://#{node.ckan.db_username}:#{node.ckan.db_password}@#{node.ckan.db_address}/#{node.ckan.db_datastore_name}",
-    'solr_url' => "http://#{node.ckan.solr_url}:8983/solr",
-    'ckan_plugins' => 'stats text_view image_view recline_view nasa_ace',
-    'ckan_default_views' => 'image_view text_view recline_view nasa_ace',
-    'ckan_site_title' => 'DEV knife NASA Arctic Collaborative Environment',
-    'ckan_site_logo_path' => '/base/images/ace_title.png',
-    'ckan_site_favicon' => '/base/images/ace_logo.png',
-    'ckan_datapusher_url' => 'http://127.0.0.1:8800/'
-  })
-  action :create
-end
-
-bash 'install NASA ACE theme' do
-  code '/usr/lib/ckan/default/bin/python setup.py develop'
-  cwd '/usr/lib/ckan/default/src/ckanext-nasa_ace'
-end
-
-execute 'init db' do
-  command 'ckan db init'
-  notifies :create, 'file[init-db]', :immediately
-  retries 1
-  retry_delay 5
-  not_if { ::File.exist?('/root/initialized-db') }
-end
-
-file 'init-db' do
-  path '/root/initialized-db'
-  action :nothing
-  notifies :restart, 'httpd_service[ckan]', :immediately
 end
